80996bb43f1502c34924ed82d001c52c1173256f
roux <yvan.roux@inrae.fr>
2023-03-19 18:35:17 +0100

    intégration bootstrap vue

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,12 @@
 <script>
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import Vue from 'vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 export default {
   name: 'App',
