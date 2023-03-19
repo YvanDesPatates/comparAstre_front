137f25dd2c640619d762d8f91e7f0587fd1635ec
roux <yvan.roux@inrae.fr>
2023-03-19 18:35:44 +0100

    formulaire de création fonctionnel mais incomplet

--- src/components/CreateAstre.vue
+++ src/components/CreateAstre.vue
@@ -1,61 +1,68 @@
 <template>
-  <form @submit.prevent="submitFrom">
+  <div id="form">
+    <b-form-group
+        id="group"
+        label="Création d'un nouvel astre"
+        :invalid-feedback="invalidFeedback"
+        :state="formOK"
+    >
 
-    <div v-if="errors.length">
-      <ul>
-        <li class="error" v-for="error in errors" :key="error">{{ error }}</li>
-      </ul>
-    </div>
+      <div class="titre"> image</div>
+      <b-form-input size="sm"
+                    class="input"
+                    v-model="newAstre.image"
+                    :state="imageOK ? null : false"
+                    placeholder="entrez l'url d'une image"
+                    trim></b-form-input>
 
-    <p>
-      <label for="nom">Nom</label>
-      <input
-          id="nom"
+      <div class="titre"> nom</div>
+      <b-form-input
+          size="sm"
+          class="input"
           v-model="newAstre.nom"
-          type="text"
-          name="nom"
-      >
-    </p>
+          :state="nomOK ? null : false"
+          trim></b-form-input>
 
-    <p>
-      <label for="categorie">Catégorie</label>
-      <input
-          id="categorie"
-          v-model="newAstre.categorie"
-          type="text"
-          name="categorie"
-      >
-    </p>
+      <div class="titre"> catégorie</div>
+      <b-form-input size="sm"
+                    class="input"
+                    v-model="newAstre.categorie"
+                    :state="categorieOK ? null : false"
+                    placeholder="planète, galaxie, étoile..."
+                    trim></b-form-input>
 
-    <p>
-      <label for="type">Type</label>
-      <input
-          id="type"
-          type="text"
-          name="type"
-          v-model="newAstre.type"
-      >
-    </p>
+      <div class="titre"> type</div>
+      <b-form-input size="sm"
+                    class="input"
+                    v-model="newAstre.type"
+                    placeholder="télurique, spirale, naine..."
+                    trim></b-form-input>
 
-    <p>
-      <label for="taille">Taille en km</label>
-      <input
-          id="taille"
-          type="number"
-          name="taille"
-          min="0"
-          v-model="newAstre.taille"
-      >
-    </p>
+      <div class="titre"> taille</div>
+      <b-form-input size="sm"
+                    class="input"
+                    v-model="newAstre.taille"
+                    placeholder="taille en km"
+                    type="number"
+                    min="0"
+                    :state="tailleOK ? null : false"
+                    trim></b-form-input>
 
-    <p>
-      <input
-          type="submit"
-          value="Submit"
-      >
-    </p>
+      <div class="titre"> distance Terre</div>
+      <b-form-input size="sm"
+                    class="input"
+                    v-model="newAstre.type"
+                    placeholder="son éloignement à la Terre en ua (unité astronomique)"
+                    trim></b-form-input>
 
-  </form>
+    </b-form-group>
+
+    <b-button
+        id="submit"
+        variant="outline-success"
+        @click="submitFrom"> créer </b-button>
+  </div>
+
 </template>
 
 <script>
@@ -64,22 +71,41 @@ import axios from "axios";
 export default {
   name: "CreateAstre",
 
+  computed: {
+    nomOK() {
+      return this.newAstre.nom !== ""
+    },
+    categorieOK() {
+      return this.newAstre.categorie !== ""
+    },
+    imageOK(){
+      return this.newAstre.image !== ""
+    },
+    tailleOK(){
+      return this.newAstre.taille !== ""
+    },
+    formOK() {
+      return this.nomOK && this.categorieOK && this.imageOK && this.tailleOK
+    },
+  },
+
   data() {
     return {
-      errors: [],
+      invalidFeedback: 'les champs image, nom, categorie et taille sont obligatoire',
       newAstre: {
+        image: "",
         auteur: "Anonyme", //le lier au nom de l'utilisateur connecté
-        nom: null,
-        categorie: null,
+        nom: "",
+        categorie: "",
         type: null,
-        taille: null
+        taille: "null"
       }
     }
   },
 
-  methods:{
-    async submitFrom(){
-      if (this.newAstre.nom && this.newAstre.categorie){
+  methods: {
+    async submitFrom() {
+      if (this.formOK) {
         let config = {
           baseURL: 'http://localhost:3000',
           method: 'post',
@@ -93,33 +119,46 @@ export default {
           const response = await axios(config);
           console.log("apres appel")
 
-          return  response.data.code === 201
+          return response.data.code === 201
         } catch (error) {
           console.log(error);
         }
         return true
       }
 
-      this.errors = []
-      if (!this.newAstre.nom){
-        this.errors.push("Le champ nom est obligatoire")
-      }
-
-      if (!this.newAstre.categorie){
-        this.errors.push("Le champ catégorie est obligatoire")
-      }
       return false;
     }
-}
+  }
 }
 </script>
 
 <style scoped>
-  .error{
-    color: indianred;
-  }
+#form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: white;
+}
 
-  label{
-    color: white;
-  }
+#group {
+  margin-left: 2%;
+  margin-top: 2%;
+  width: 40%;
+}
+
+#submit{
+  margin: 2% 0;
+  color: #ff8d00;
+  border-color: #ff8d00;
+}
+
+.titre {
+  margin-top: 5%;
+  color: white;
+  text-align: center;
+}
+
+label {
+  color: white;
+}
 </style>
