a3ef72965a6c6bd883d3393ffac7d9cf1981f5f6
Julie Nussbaumer <jnussbaumer@libriciel.coop>
2023-03-29 20:06:25 +0200

    ajout cookie

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Vuex from "vuex";
+import Cookies from "js-cookie";
 
 Vue.use(Vuex)
 Vue.config.productionTip = false
@@ -23,7 +24,14 @@ const store = new Vuex.Store({
   }
 })
 
-console.log(store.getters.user);
+store.subscribe((mutation, state) => {
+  Cookies.set('store', JSON.stringify(state))
+})
+
+const cookieState = Cookies.get('store')
+if (cookieState) {
+  store.replaceState(JSON.parse(cookieState))
+}
 
 new Vue({
   el: '#app',
